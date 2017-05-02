2a329df02a91adb8f025af11d1a833b4782b9431
zeeshanshanif <zee81zee@yahoo.com>
2017-05-02 15:53:06 +0500

    Implemented show more description feature on home screen

--- client/src/containers/home/Home.tsx
+++ client/src/containers/home/Home.tsx
@@ -3,46 +3,139 @@ import './Home.css';
 import Paper from 'material-ui/Paper';
 
 import FlatButton from 'material-ui/FlatButton';
-import { RaisedButton } from 'material-ui'
+import {RaisedButton} from 'material-ui'
 
 class Home extends React.Component<any, any> {
+    state = {
+        panelArray: [
+            {
+                title: 'Light Industrial Worker',
+                location: 'Elements, outside',
+                duration: 'part-Time',
+                hrs: '8AM-4PM',
+                compensation: 'Competitive hourly rate with overtime available.',
+                desc: `Performing work in a storage/warehouse settinfg managing different capabilities which may or may not include: product packing,
+                fulfillment, loading, unloading, transportation, shipping/delivery, and operation of machinery.`,
+                showMore: false
+            },
+            {
+                title: 'Light Industrial Worker',
+                location: 'Elements, outside',
+                duration: 'part-Time',
+                hrs: '8AM-4PM',
+                compensation: 'Competitive hourly rate with overtime available.',
+                desc: `Performing work in a storage/warehouse settinfg managing different capabilities which may or may not include: product packing,
+                fulfillment, loading, unloading, transportation, shipping/delivery, and operation of machinery.`,
+                showMore: false
+            },
+            {
+                title: 'Light Industrial Worker',
+                location: 'Elements, outside',
+                duration: 'part-Time',
+                hrs: '8AM-4PM',
+                compensation: 'Competitive hourly rate with overtime available.',
+                desc: `Performing work in a storage/warehouse settinfg managing different capabilities which may or may not include: product packing,
+                fulfillment, loading, unloading, transportation, shipping/delivery, and operation of machinery.`,
+                showMore: false
+            },
+            {
+                title: 'Light Industrial Worker',
+                location: 'Elements, outside',
+                duration: 'part-Time',
+                hrs: '8AM-4PM',
+                compensation: 'Competitive hourly rate with overtime available.',
+                desc: `Performing work in a storage/warehouse settinfg managing different capabilities which may or may not include: product packing,
+                fulfillment, loading, unloading, transportation, shipping/delivery, and operation of machinery.`,
+                showMore: false
+            },
+            {
+                title: 'Light Industrial Worker',
+                location: 'Elements, outside',
+                duration: 'part-Time',
+                hrs: '8AM-4PM',
+                compensation: 'Competitive hourly rate with overtime available.',
+                desc: `Performing work in a storage/warehouse settinfg managing different capabilities which may or may not include: product packing,
+                fulfillment, loading, unloading, transportation, shipping/delivery, and operation of machinery.`,
+                showMore: false
+            },
+            {
+                title: 'Light Industrial Worker',
+                location: 'Elements, outside',
+                duration: 'part-Time',
+                hrs: '8AM-4PM',
+                compensation: 'Competitive hourly rate with overtime available.',
+                desc: `Performing work in a storage/warehouse settinfg managing different capabilities which may or may not include: product packing,
+                fulfillment, loading, unloading, transportation, shipping/delivery, and operation of machinery.`,
+                showMore: false
+            }
+        ],
+    };
     panelArray: any;
+
+    showDescription(obj: any, ind: any) {
+        this.state.panelArray[ind].showMore = !this.state.panelArray[ind].showMore;
+        this.setState({panelArray: this.state.panelArray})
+    };
+
     render() {
-        let panelsList = [
-            { title: 'Light Industrial Worker', location: 'Elements, outside', duration: 'part-Time', hrs: '8AM-4PM' },
-            { title: 'Light Industrial Worker', location: 'Elements, outside', duration: 'part-Time', hrs: '8AM-4PM' },
-            { title: 'Light Industrial Worker', location: 'Elements, outside', duration: 'part-Time', hrs: '8AM-4PM' },
-            { title: 'Light Industrial Worker', location: 'Elements, outside', duration: 'part-Time', hrs: '8AM-4PM' },
-            { title: 'Light Industrial Worker', location: 'Elements, outside', duration: 'part-Time', hrs: '8AM-4PM' },
-            { title: 'Light Industrial Worker', location: 'Elements, outside', duration: 'part-Time', hrs: '8AM-4PM' },
-        ];
+
         this.panelArray = (
-            panelsList.map((d, i) => {
+            this.state.panelArray.map((jobObj: any, i: number) => {
                 return (
                     <Paper zDepth={2} key={i} className="apply-paper">
                         <div className="paper-container">
-                            <h3>Light Industrial Worker</h3>
+
+                            {jobObj.showMore ?
+                                <img src={require("../../assets/window-close.svg")} className="cross-btn"
+                                     onClick={this.showDescription.bind(this, jobObj, i)}/>
+                                : null}
+
+                            <h3>{jobObj.title}</h3>
                             <div className="text-left">
                                 <div>
-                                    <span>Location: </span><span>Elements, outside</span>
+                                    <span className="label-title">Location: </span>
+                                    <span>{jobObj.location}</span>
                                 </div>
                                 <div>
-                                    <span>Duration: </span><span>Part-Time</span>
+                                    <span className="label-title">Duration: </span>
+                                    <span>{jobObj.duration}</span>
                                 </div>
                                 <div>
-                                    <span>Hours: </span><span>8AM-4PM</span>
+                                    <span className="label-title">Hours: </span>
+                                    <span>{jobObj.hrs}</span>
                                 </div>
+
+                                {jobObj.showMore ?
+                                    <div className="showMoreDesc-container">
+                                        <div>
+                                            <span className="label-title">Description: </span>
+                                            <span>{jobObj.desc}</span>
+                                        </div>
+                                        <div>
+                                            <span className="label-title">Compensation: </span>
+                                            <span>{jobObj.compensation}</span>
+                                        </div>
+                                    </div>
+                                    : null}
+
                             </div>
                         </div>
                         <div className="paper-bar"></div>
-                        <div className="text-left">
-                            <FlatButton label="MORE" />
-                            <FlatButton label="APPLY" />
-                        </div>
+
+                        {jobObj.showMore ?
+                            <div className="text-left footer-container">
+                                <FlatButton label="APPLY" className="apply-job-btn"/>
+                            </div>
+                            :
+                            <div className="text-left footer-container">
+                                <FlatButton label="MORE" onClick={this.showDescription.bind(this, jobObj, i)}/>
+                                <FlatButton label="APPLY"/>
+                            </div>
+                        }
                     </Paper>
                 )
             })
-        )
+        );
         return (
             <div>
                 {this.props.children}
@@ -50,12 +143,12 @@ class Home extends React.Component<any, any> {
                     <h2 className="title"> We span the gap</h2>
                     <div className="title-desc">
                         InterSpan provides quality employees to companies that need them.
-                            </div>
+                    </div>
 
                     <p className="apply-desc">
                         We have open positions now, and applying is easy.
-                            </p>
-                    <RaisedButton primary label="Apply" onTouchTap={this.props.logout} />
+                    </p>
+                    <RaisedButton primary label="Apply" onTouchTap={this.props.logout}/>
                     <h1>Positions</h1>
                 </div>
                 {this.panelArray}
