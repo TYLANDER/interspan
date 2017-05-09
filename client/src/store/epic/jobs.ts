d648b522f22cba7a04e598b081fdf2578ca05364
zeeshanshanif <zee81zee@yahoo.com>
2017-05-09 18:34:04 +0500

    Fixed api for get all jobs

--- client/src/store/epic/jobs.ts
+++ client/src/store/epic/jobs.ts
@@ -8,7 +8,7 @@ export default class JobEpic {
     static getAllJobsEpic = (action$: ActionsObservable<any>) =>
         action$.ofType(JobActions.GET_ALL_JOBS)
             .switchMap(({payload}) => {                
-                return HttpService.get('http://interspanresources-164914.appspot.com/jobs')
+                return HttpService.get('http://interspanresources-164914.appspot.com/api/jobs/jobs')
                     .switchMap(({response}) => {
                         return Observable.of({
                             type: JobActions.GET_ALL_JOBS_SUCCESS,
