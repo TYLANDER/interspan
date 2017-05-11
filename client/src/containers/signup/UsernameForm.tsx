2dd5d0969c31d774914145ae174a01e15e81f7e5
zeeshanshanif <zee81zee@yahoo.com>
2017-05-11 16:49:47 +0500

    Partially implemented form validation and UI fixes

--- client/src/containers/signup/UsernameForm.tsx
+++ client/src/containers/signup/UsernameForm.tsx
@@ -2,14 +2,37 @@ import * as React from 'react';
 import {TextField} from "material-ui";
 
 class UsernameForm extends React.Component<any, any> {
-
+ isFullName(value:any) {
+        if(value.trim()==""){
+            this.setState({
+                email_error:true,
+                err_message:"Email field is required",
+                email_success:false
+            })
+            return;
+        }
+       let pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+       let isValidEmail = pattern.test(value);
+       if(!isValidEmail)
+       {
+           this.setState({
+                email_error:true,
+                err_message:"Please enter valid email address",
+                email_success:false
+            })
+            return;
+       }
+        this.setState({
+            email_error:false,
+            email_success:true
+        })
+    }
     render() {
         return (
             <div className="fields-container">
                 <label className="title">Enter your name</label>
                 <TextField
                     hintText=""
-                    errorText=""
                     fullWidth={true}
                     floatingLabelText="First Name"
                 />
