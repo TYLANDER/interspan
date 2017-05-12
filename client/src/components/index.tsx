73d515a58974d670c2a93c413ac87b4d3b5ff9ea
zeeshanshanif <zee81zee@yahoo.com>
2017-05-12 16:26:41 +0500

    Renamed directory structure for static pages

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,7 +1,7 @@
 import NavBar from './navbar/NavBar';
-import AboutUs from './about_us/AboutUs';
-import EmployeeInfo from './employee_info/EmployeeInfo';
-import HireFor from './hire_for/HireFor';
+import AboutUs from './about-us/AboutUs';
+import EmployeeInfo from './employee-info/EmployeeInfo';
+import HireFor from './hire-for/HireFor';
 
 export {
     NavBar,
