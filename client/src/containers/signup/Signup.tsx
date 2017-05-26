5fbdf04746bca2c88976dd6da05dbcbd378c61de
zeeshanshanif <zee81zee@yahoo.com>
2017-05-26 19:21:13 +0500

    Refactor signup form

--- client/src/containers/signup/Signup.tsx
+++ client/src/containers/signup/Signup.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './Signup.css';
-import { Paper, FlatButton ,CircularProgress} from 'material-ui';
+import { Paper, CircularProgress } from 'material-ui';
 import { indigo900 } from 'material-ui/styles/colors';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -27,10 +27,10 @@ class Signup extends React.Component<any, any> {
     });
 
     state: any = {
-        currentScreen: 'UsernameForm',
         isAuthenticated: false,
         userObj: {},
-        loading:false
+        loading: false,
+        step: 2
     };
 
     componentWillMount() {
@@ -40,82 +40,54 @@ class Signup extends React.Component<any, any> {
     handleHomePage() {
         browserHistory.push('/');
     }
-    componentWillReceiveProps(newProps:any){
-        newProps.authObj.isProcessing?this.setState({loading:true}):this.setState({loading:false});
+    componentWillReceiveProps(newProps: any) {
+        newProps.authObj.isProcessing ? this.setState({ loading: true }) : this.setState({ loading: false });
         newProps.authObj.isRegistered ? browserHistory.push('/job') : null;
-
     }
+    
     handleJobPage = () => {
         this.props.signUp(this.state.userObj);
-        
+
     }
-    collectFormData = (user: any, error: any) => {
-        let users = Object.assign(this.state.userObj, user);
-        this.setState({ userObj: users });
+
+    handleNext = (values:any) => {
+        let users = Object.assign(this.state.userObj, values);
+        //returning form value
+        console.log(users);
+        if(this.state.step == 2 )
+            // call db
+            alert("You are done");
+
+        else
+            this.setState({
+                step: this.state.step + 1
+            })
     }
-    swapScreen(screenName: any) {
-        let changedScreen = '';
-        /*
-        if (screenName == "back" && this.state.currentScreen == "UsernameForm"){
-            changedScreen = "UsernameForm";
-            this.handleHomePage();
+
+    formCard = () => {
+        switch (this.state.step) {
+            case 0: return <UsernameForm  clickEvent={this.handleNext} />;
+            case 1: return <UserEmailForm  clickEvent={this.handleNext} />
+            case 2: return <UserSSNForm clickEvent={this.handleNext} />
+            default: return <UsernameForm  clickEvent={this.handleNext} />;
         }
-        else if (screenName == "back" && this.state.currentScreen == "UserEmailForm")
-            changedScreen = "UsernameForm";
-        else if (screenName == "back" && this.state.currentScreen == "UserSSNForm")
-            changedScreen = "UserEmailForm";
-             */
-
-        if (screenName === 'continue' && this.state.currentScreen === 'UsernameForm')
-            changedScreen = 'UserEmailForm';
-        else if (screenName === 'continue' && this.state.currentScreen === 'UserEmailForm')
-            changedScreen = 'UserSSNForm';
-        else if (screenName === 'continue' && this.state.currentScreen === 'UserSSNForm') {
-            changedScreen = 'UserSSNForm';
-            this.handleJobPage();
-        }
-
-        this.setState({ currentScreen: changedScreen });
     }
-
     render() {
 
-        let formElement: any = (
-            <div>
-                {(this.state.currentScreen == 'UsernameForm') ? <UsernameForm collection={this.collectFormData} /> :
-                    (this.state.currentScreen == 'UserEmailForm') ? <UserEmailForm collection={this.collectFormData} /> : <UserSSNForm collection={this.collectFormData} />}
-
-                <div className="footer-container">
-                    {/*<FlatButton label="Back"
-                        className="back-btn"
-                        secondary={true}
-                        labelStyle={{color: '#2e469e',fontSize:"16px"}}
-                        icon={<img src={require("../../assets/angle-left.svg")} className="btn-icon back" alt="logo" />}
-                        onClick={this.swapScreen.bind(this, "back")}/>*/}
-
-                    <FlatButton label="Continue"
-                        className="continue-btn"
-                        labelPosition="before"
-                        primary={true}
-                        labelStyle={{ color: '#2e469e', fontSize: '16px' }}
-                        icon={<img src={require('../../assets/angle-right.svg')} className="btn-icon continue" alt="logo" />}
-                        onClick={this.swapScreen.bind(this, 'continue')} />
-                </div>
-            </div>
-        );
         return (
             <MuiThemeProvider muiTheme={this.muiTheme}>
-                {this.state.loading?<CircularProgress size={80} thickness={5} color="rgb(45, 69, 158)" style={{ position: 'absolute', textAlign: 'center', margin: '0 auto', left: 0, right: 0 }} />:<div className="signup-container">
-                    <div className="paper-container">
-                        <Paper zDepth={2} className="md-screen">
-                            {formElement}
-                        </Paper>
-                        <div className="sm-screen">
-                            {formElement}
+                {this.state.loading ? <CircularProgress size={80} thickness={5} color="rgb(45, 69, 158)"
+                    style={{ position: 'absolute', textAlign: 'center', margin: '0 auto', left: 0, right: 0 }} /> : <div className="signup-container">
+                        <div className="paper-container">
+                            <Paper zDepth={2} className="md-screen">
+                                {this.formCard()}
+                            </Paper>
+                            <div className="sm-screen">
+                                {this.formCard()}
+                            </div>
                         </div>
-                    </div>
-                </div>}
-                
+                    </div>}
+
             </MuiThemeProvider>
         );
     }
